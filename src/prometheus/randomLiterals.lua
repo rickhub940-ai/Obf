10a8b4787bf51a0831edd881e768887ccb73b19a
rickhub940-ai <rickhub940@gmail.com>
2026-08-11 14:56:38 +0700

    Refactor random literals generation functions

--- src/prometheus/randomLiterals.lua
+++ src/prometheus/randomLiterals.lua
@@ -1,83 +1,46 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- Library for Creating Random Literals
-
 local Ast = require("prometheus.ast")
 local RandomStrings = require("prometheus.randomStrings")
 
 local RandomLiterals = {}
 
--- =========================================================
--- Random Junk Characters
--- A-Z / a-z / # $ @ ? !
--- =========================================================
-
-local function randomLetters(length)
-    local chars =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ..
-        "abcdefghijklmnopqrstuvwxyz" ..
-        "#$@?!"
-
-    local result = {}
-
-    for i = 1, length do
-        local index = math.random(1, #chars)
-        result[i] = chars:sub(index, index)
+local function callNameGenerator(generatorFunction, ...)
+    if type(generatorFunction) == "table" then
+        generatorFunction = generatorFunction.generateName
     end
-
-    return table.concat(result)
+    return generatorFunction(...)
 end
-
--- =========================================================
--- Random String
--- Long random junk string
--- =========================================================
 
 function RandomLiterals.String(pipeline)
-    local value = randomLetters(
-        math.random(100, 500)
+    return Ast.StringExpression(
+        callNameGenerator(
+            pipeline.namegenerator,
+            math.random(1, 4096)
+        )
     )
-
-    return Ast.StringExpression(value)
 end
-
--- =========================================================
--- Random Dictionary
--- =========================================================
 
 function RandomLiterals.Dictionary()
     return RandomStrings.randomStringNode(true)
 end
 
--- =========================================================
--- Random Number
--- Keep numbers small
--- =========================================================
-
 function RandomLiterals.Number()
     return Ast.NumberExpression(
-        math.random(0, 25)
+        math.random(-8388608, 8388607)
     )
 end
 
--- =========================================================
--- Random Any
--- =========================================================
-
 function RandomLiterals.Any(pipeline)
+    local type = math.random(1, 3)
 
-    local randomType = math.random(1, 3)
-
-    if randomType == 1 then
+    if type == 1 then
         return RandomLiterals.String(pipeline)
 
-    elseif randomType == 2 then
+    elseif type == 2 then
         return RandomLiterals.Number()
 
-    else
+    elseif type == 3 then
         return RandomLiterals.Dictionary()
     end
-
 end
 
 return RandomLiterals
