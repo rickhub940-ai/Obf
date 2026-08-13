9e8fdf8cbcb685115b96bf2bd501107145778e67
rickhub940-ai <rickhub940@gmail.com>
2026-08-13 18:34:43 +0700

    Update namegenerators.lua

--- src/prometheus/namegenerators.lua
+++ src/prometheus/namegenerators.lua
@@ -4,5 +4,4 @@ return {
 	Il = require("prometheus.namegenerators.Il");
 	Number = require("prometheus.namegenerators.number");
 	Confuse = require("prometheus.namegenerators.confuse");
-	BinaryStyle = require("prometheus.namegenerators.binarystyle");
 }
