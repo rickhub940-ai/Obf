4f54bd268a0e6f573768e78f0229732a01000a25
rickhub940-ai <rickhub940@gmail.com>
2026-08-21 17:58:20 +0700

    Change NameGenerator from 'BinaryStyle' to 'Mangled'

--- src/presets.lua
+++ src/presets.lua
@@ -61,7 +61,7 @@ return {
         -- For minifying no VarNamePrefix is applied
         VarNamePrefix = "";
         -- Name Generator for Variables
-        NameGenerator = "BinaryStyle";
+        NameGenerator = "Mangled";
         -- No pretty printing
         PrettyPrint = false;
         -- Seed is generated based on current time
