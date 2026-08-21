150828b874d624cdbb8936c6c4c4d7b2173fdea7
rickhub940-ai <rickhub940@gmail.com>
2026-08-21 17:56:50 +0700

    Enhance name generation with special characters
    Added support for special characters in name generation.

--- src/prometheus/namegenerators/mangled.lua
+++ src/prometheus/namegenerators/mangled.lua
@@ -1,26 +1,38 @@
--- This Script is Part of the Prometheus Obfuscator by Levno_710
---
--- namegenerators/mangled.lua
---
--- This Script provides a function for generation of mangled names
-
-
 local util = require("prometheus.util");
 local chararray = util.chararray;
 
 local idGen = 0
-local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_");
 local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_");
+local SpecialChars = chararray("#@$!%^&*+-=~`|:;<>?/");
 
-return function(id, scope)
-	local name = ''
-	local d = id % #VarStartDigits
-	id = (id - d) / #VarStartDigits
-	name = name..VarStartDigits[d+1]
-	while id > 0 do
-		local d = id % #VarDigits
-		id = (id - d) / #VarDigits
-		name = name..VarDigits[d+1]
-	end
-	return name
+return function(id, scope, useSpecial)
+    local name = ''
+    local alphabet
+    
+    if useSpecial then
+        -- ใช้สัญลักษณ์พิเศษด้วย (แต่ต้องเก็บใน table)
+        alphabet = VarDigits .. SpecialChars
+        local d = id % #VarStartDigits
+        id = (id - d) / #VarStartDigits
+        name = name..VarStartDigits[d+1]
+        while id > 0 do
+            local d = id % #alphabet
+            id = (id - d) / #alphabet
+            name = name..alphabet[d+1]
+        end
+        -- คืนค่าแบบ table access
+        return "['"..name.."']"
+    else
+        -- แบบปกติ (ชื่อตัวแปร)
+        local d = id % #VarStartDigits
+        id = (id - d) / #VarStartDigits
+        name = name..VarStartDigits[d+1]
+        while id > 0 do
+            local d = id % #VarDigits
+            id = (id - d) / #VarDigits
+            name = name..VarDigits[d+1]
+        end
+        return name
+    end
 end
