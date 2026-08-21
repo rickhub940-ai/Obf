1a3dd07bd757f7b3ae1cdb76279d99b95b13c6e0
rickhub940-ai <rickhub940@gmail.com>
2026-08-21 17:35:24 +0700

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -61,7 +61,7 @@ return {
         -- For minifying no VarNamePrefix is applied
         VarNamePrefix = "";
         -- Name Generator for Variables
-        NameGenerator = "MangledShuffled";
+        NameGenerator = "BinaryStyle";
         -- No pretty printing
         PrettyPrint = false;
         -- Seed is generated based on current time
